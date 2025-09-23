6408d7039fbaa2dde9aa86af29c60ff7c6e1488d
swapnilnewvision <swapnil@sports-techsolutions.com>
2025-09-23 15:34:07 +0530

    test api

--- app/Filament/Admin/Resources/PostReactionResource/Pages/EditPostReaction.php
+++ app/Filament/Admin/Resources/PostReactionResource/Pages/EditPostReaction.php
@@ -59,3 +59,5 @@ class EditPostReaction extends EditRecord
 
 
 
+
+
--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Simple test endpoint to verify API and CORS (versioned)
+Route::prefix('v1')->group(function () {
+    Route::get('/ping', function () {
+        return response()->json([
+            'ok' => true,
+            'message' => 'pong',
+            'time' => now()->toIso8601String(),
+        ]);
+    });
+});
+
 
